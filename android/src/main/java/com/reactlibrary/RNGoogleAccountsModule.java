1164014129492703d0fc70c8c7b82443ef66705e
Gokul T P <tp.gokul@gmail.com>
2018-05-08 09:26:34 +0530

    promisified

--- android/src/main/java/com/reactlibrary/RNGoogleAccountsModule.java
+++ android/src/main/java/com/reactlibrary/RNGoogleAccountsModule.java
@@ -12,34 +12,44 @@ import java.util.*;
 
 public class RNGoogleAccountsModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNGoogleAccountsModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
-
-  @Override
-  public String getName() {
-    AccountManager manager = AccountManager.get(this);
-    Account[] accounts = manager.getAccountsByType("com.google");
-    List<String> possibleEmails = new LinkedList<String>();
-
-    for (Account account : accounts) {
-        // TODO: Check possibleEmail against an email regex or treat
-        // account.name as an email address only for certain account.type
-        // values.
-        possibleEmails.add(account.name);
+    public RNGoogleAccountsModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
     }
 
-    if (!possibleEmails.isEmpty() && possibleEmails.get(0) != null) {
-        String email = possibleEmails.get(0);
-        String[] parts = email.split("@");
-        if (parts.length > 0 && parts[0] != null)
-            return parts[0];
-        else
-            return null;
-    } else
-        return null;
-  }
+    @Override
+    public String getName() {
+        return "RNGoogleAccountsModule";
+    }
+
+    @ReactMethod
+    public void isAvailable(final Promise promise) {
+        try {
+            AccountManager manager = AccountManager.get(this);
+            Account[] accounts = manager.getAccountsByType("com.google");
+            List<String> possibleEmails = new LinkedList<String>();
+
+            for (Account account : accounts) {
+                // TODO: Check possibleEmail against an email regex or treat
+                // account.name as an email address only for certain account.type
+                // values.
+                possibleEmails.add(account.name);
+            }
+
+            if (!possibleEmails.isEmpty() && possibleEmails.get(0) != null) {
+                String email = possibleEmails.get(0);
+                String[] parts = email.split("@");
+                if (parts.length > 0 && parts[0] != null)
+                    promise.resolve(parts[0);
+                else
+                    promise.resolve(null);
+            } else
+                 promise.resolve(null);        
+        } catch (Exception ex) {
+            promise.reject("ERR_UNEXPECTED_EXCEPTION", ex);
+        }
+    }
+
 }
