55c03903c70d453fc0f58e1546ce3efb9a72c981
Gokul T P <tp.gokul@gmail.com>
2018-05-08 11:28:30 +0530

    changed package

--- android/src/main/java/com/reactlibrary/RNGoogleAccountsModule.java
+++ android/src/main/java/com/reactlibrary/RNGoogleAccountsModule.java
@@ -29,7 +29,7 @@ public class RNGoogleAccountsModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void getAccountEmail(final Promise promise) {
         try {
-            AccountManager manager = AccountManager.get(this);
+            AccountManager manager = AccountManager.get(reactContext);
             Account[] accounts = manager.getAccountsByType("com.google");
             List<String> possibleEmails = new LinkedList<String>();
 
@@ -42,11 +42,8 @@ public class RNGoogleAccountsModule extends ReactContextBaseJavaModule {
 
             if (!possibleEmails.isEmpty() && possibleEmails.get(0) != null) {
                 String email = possibleEmails.get(0);
-                String[] parts = email.split("@");
-                if (parts.length > 0 && parts[0] != null)
-                    promise.resolve(parts[0);
-                else
-                    promise.resolve(null);
+                promise.resolve(email);
+                
             } else
                  promise.resolve(null);        
         } catch (Exception ex) {
